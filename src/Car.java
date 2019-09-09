147be51a35dec4259ffe8b22379fb16361c44fc9
Aidan <aidan.megee@my.jcu.edu.au>
2019-09-09 11:01:48 +1000

    Add setters and getter methods for Car class. Add initial test code for setters and getters in CarTest class.

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,43 @@
 public class Car {
+
+    int positionOnRoad = 0; //index 0 for car position start
+    int roadLength = 10;
+    String road1;
+    String road2;
+
+    /*setters and getters for car class*/
+
+    public int getPositionOnRoad() {
+        return positionOnRoad;
+    }
+
+    public int getRoadLength() {
+        return roadLength;
+    }
+
+    public String getRoad1() {
+        return road1;
+    }
+
+    public String getRoad2() {
+        return road2;
+    }
+
+    public void setRoad1(String road1) {
+        this.road1 = road1;
+    }
+
+    public void setRoad2() {
+        this.road2 = road2;
+    }
+
+    public void setRoadLength(int roadLength) {
+        this.roadLength = roadLength;
+    }
+
+    public void setPositionOnRoad(int positionOnRoad) {
+        this.positionOnRoad = positionOnRoad;
+    }
+
+
 }
---
+++ src/CarTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+
+    @Test
+    void carTest() {
+        Car car = new Car();
+        car.setPositionOnRoad(0);
+
+    }
+
+
+
+}
