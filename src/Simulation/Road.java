1b9ba727e648aa59d1c0079a1651c80fdfaee306
aidanmegee <aidan.megee@my.jcu.edu.au>
2019-09-11 13:59:07 +1000

    Add roadId field to Road class to track what road car is on. Add setter and getter method for roadId. Add test code for getter methods road length and roadId.

--- src/Simulation/Road.java
+++ src/Simulation/Road.java
@@ -2,24 +2,24 @@ package Simulation;
 
 public class Road { //Road class extends Car class
 
-    private int roadLength; //index 0 to 4
-    private Car car;
+    private int roadLength = 5; //index 0 to 4
+    private int roadId = 1;
 
-    public Road(int roadLength, Car car) { //constructor for Road class
-        this.roadLength = roadLength;
-        this.car = car;
+    public Road(int roadId) { //constructor for Road class
+        this.roadId = roadId;
+
     }
-
-
-
-    public int roadLength() { //road length method for length of road
-        roadLength = String.valueOf(roadLength).length(); //road length is 0, 1, 2, 3, 4
-        return roadLength; //returns road length
-    }
-
-    /*get and set road length for Road class*/
+    /*setters and getters for Road class*/
     public int getRoadLength() {
-        return roadLength();
+        return roadLength;
+    }
+
+    public int getRoadId() {
+        return roadId;
+    }
+
+    public void setRoadId(int roadId) {
+        this.roadId = roadId;
     }
 
 
--- src/Simulation/RoadTest.java
+++ src/Simulation/RoadTest.java
@@ -8,36 +8,13 @@ class RoadTest {
 
 
     @Test
-    void roadTest() { //test for road length in Road Class
-        Road road = new Road();
-        road.setRoadLength(5);
-        road.getRoadLength(5);
-        assertEquals(5, road.roadLength());
+    void roadTest() { //test for getters in Road Class
+        Road road = new Road(1);
+        assertEquals(5, road.getRoadLength());
+        assertEquals(1, road.getRoadId());
 
     }
 
-    @Test
-    void getPositionOnRoad() {
-        Road road = new Road();
-        assertEquals(0, road.positionOnRoad);
-    }
 
-    @Test
-    void setPositionOnRoad() {
-        Road road = new Road();
-        assertEquals(0, road.positionOnRoad);
-    }
-
-    @Test
-    void getRoad1() {
-        Road road = new Road();
-        assertEquals("Road 1", road.road1 );
-    }
-
-    @Test
-    void getRoad2() {
-        Road road = new Road();
-        assertEquals("Road 2", road.road2);
-    }
 
 }
