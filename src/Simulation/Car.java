1728d92b67fedd098712f8652e06f44259c32977
aidanmegee <aidan.megee@my.jcu.edu.au>
2019-09-11 10:43:21 +1000

    Add constructor to Car class. Add code to make Road1 and road2 objects inside Car class.

--- src/Simulation/Car.java
+++ src/Simulation/Car.java
@@ -2,10 +2,15 @@ package Simulation;
 
 public class Car { //Car class
 
-    int positionOnRoad = 0; //index 0 for car position start
-    String vehicle = "Car";
-    String road1 = "Road 1";
-    String road2 = "Road 2";
+    private int positionOnRoad; //index 0 for car position start
+    private Road road1; //Road object
+    private Road road2; //Road 2 objects
+
+    public Car(int positionOnRoad, Road road1, Road road2) { //constructor for Car class
+        this.positionOnRoad = positionOnRoad;
+        this.road1 = road1;
+        this.road2 = road2;
+    }
 
     /*setters and getters for car class*/
 
@@ -13,27 +18,20 @@ public class Car { //Car class
         return positionOnRoad;
     }
 
-    public String getVehicle() {
-        return vehicle;
-    }
-
-    public String getRoad1() {
+    public Road getRoad1() {
         return road1;
     }
 
-    public String getRoad2() {
+    public Road getRoad2() {
         return road2;
     }
 
-    public void setVehicle(String vehicle) {
-        this.vehicle = vehicle;
-    }
 
-    public void setRoad1(String road1) {
+    public void setRoad1(Road road1) {
         this.road1 = road1;
     }
 
-    public void setRoad2(String road2) {
+    public void setRoad2(Road road2) {
         this.road2 = road2;
     }
 
@@ -43,21 +41,12 @@ public class Car { //Car class
     }
 
 
-    public void carMove(String road1, String road2, String vehicle, int positionOnRoad) {
+    public void carMove(Road road1, Road road2, int positionOnRoad) {
         this.road1 = road1;
         this.road2 = road2;
-        this.vehicle = vehicle;
         this.positionOnRoad = positionOnRoad;
 
-        positionOnRoad = 0; //initial position on road
-        System.out.println(vehicle + " is positioned at " + road1 + " on segment " + positionOnRoad);
-        if (positionOnRoad < 4) {
-            System.out.println(vehicle + " is now positioned at " + road1 + " on segment " + positionOnRoad);//prints new position on road
-            ++positionOnRoad; //increment position on road
-        }
-        if (positionOnRoad == 5) {
-            System.out.println(vehicle + " is now positioned at " + road2 + " on segment " + positionOnRoad); //if vehicle is at position 5, move to next road
-        }
+
 
 
     }
