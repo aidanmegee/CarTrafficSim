cb54022758fc5774d3eda3ea8cb1202ccac34efa
aidanmegee <aidan.megee@my.jcu.edu.au>
2019-09-13 12:25:04 +1000

    Add code so that if lights are red at the end of road 1, car stops and waits until lights are green to move to road 2. Add UML class diagram + png to design directory. Add problem fix to working doc.

--- src/Simulation/MainSimulation.java
+++ src/Simulation/MainSimulation.java
@@ -19,56 +19,60 @@ public class MainSimulation { //main simulation class
             trafficLight.operate(); //calls operate method from traffic light class
             System.out.println("Car " + car.getId() + " is on road : " + road.getRoadId() + " on segment " + car.getPositionOnRoad() + " Traffic Lights are : " + trafficLight.getCurrentState());
             if (car.getPositionOnRoad() == 4 && road.getRoadId() == 1) {
-                while (!trafficLight.getCurrentState().equals("Red") && car.getPositionOnRoad() == 4 && road.getRoadId() == 1) {
+                if (!trafficLight.getCurrentState().equals("Red") && car.getPositionOnRoad() == 4 && road.getRoadId() == 1) {
                     road.roadChange();
                     car.setPositionOnRoad(0);
                     System.out.println("Car " + car.getId() + " is now travelling on road : " + road.getRoadId() + " on segment " + car.getPositionOnRoad());
 
-                    if (trafficLight.getCurrentState().equals("Red") && car.getPositionOnRoad() == 4) {
-                        System.out.println("Car " + car.getId() + " is currently stopped at a " + trafficLight.getCurrentState() + " light");
-                        trafficLight.operate(); //operates lights if current state is red and position is 4
-                        if (trafficLight.getCurrentState().equals("Red") && car.getPositionOnRoad() == 4 && road.getRoadId() == 1) {
-                            trafficLight.operate();
-                        } else if (trafficLight.getCurrentState().equals("Green") && car.getPositionOnRoad() == 4) {
-                            road.roadChange();
-                            car.setPositionOnRoad(0);
-                            System.out.println("Car " + car.getId() + " is now travelling on road : " + road.getRoadId() + " on segment " + car.getPositionOnRoad());
-                        }
+                }
+                while (trafficLight.getCurrentState().equals("Red") && car.getPositionOnRoad() == 4 && road.getRoadId() == 1) {
+                    System.out.println("Car " + car.getId() + " is currently stopped at a " + trafficLight.getCurrentState() + " light");
+                    System.out.println("Traffic lights are changing");
+                    trafficLight.operate();
+
+                    System.out.println("Traffic lights are now : " + trafficLight.getCurrentState());//operates lights if current state is red and position is 4 and roadId is 1
+                    if (trafficLight.getCurrentState().equals("Green") && car.getPositionOnRoad() == 4 && road.getRoadId() == 1) {
+                        road.roadChange();
+                        car.setPositionOnRoad(0);
+                        System.out.println("Car " + car.getId() + " is now travelling on road : " + road.getRoadId() + " on segment " + car.getPositionOnRoad());
+                    } else if (trafficLight.getCurrentState().equals("Red") && car.getPositionOnRoad() == 4 && road.getRoadId() == 1) {
+                        trafficLight.operate();
+                        road.setRoadId(1);
+                        car.setPositionOnRoad(4);
                     }
+                }
 
-                    boolean[] array = trafficLight.getArray();
-                    boolean change = false;
-                    while (!change && trafficLight.getCurrentState().equals("Red")) {//while value isn't false, calls operate method
-                        if (array[1]) { //checks index of array is "red"
-                            trafficLight.operate(); //calls operate method if lights are red
-                            change = true;
-                            trafficLight.operate(); //calls operate method again if lights are red
-                            if (array[0]) { //checks index of array is "green"
-                                change = false;
-                                System.out.println("Traffic lights are : " + trafficLight.getCurrentState());
-                                road.roadChange(); //changes road if traffic lights are green
+                boolean[] array = trafficLight.getArray();
+                boolean change = false;
+                while (!change && trafficLight.getCurrentState().equals("Red")) {//while value isn't false, calls operate method
+                    if (array[1]) { //checks index of array is "red"
+                        trafficLight.operate(); //calls operate method if lights are red
+                        change = true;
+                        trafficLight.operate(); //calls operate method again if lights are red
+                        if (array[0]) { //checks index of array is "green"
+                            change = false;
+                            System.out.println("Traffic lights are : " + trafficLight.getCurrentState());
+                            road.roadChange(); //changes road if traffic lights are green
 
-                                //while loop to call operate() function so traffic lights keeps trying to change if lights are red
+                            //while loop to call operate() function so traffic lights keeps trying to change if lights are red
 
-                            }
                         }
-
                     }
 
                 }
-            }
-            while (car.getPositionOnRoad() < 4 && road.getRoadId() == 2) { //Car moves while on road 2 without traffic light on road 2
-                car.carMove();
-                System.out.println("Car " + car.getId() + " is on road : " + road.getRoadId() + " on segment " + car.getPositionOnRoad());
+                while (car.getPositionOnRoad() < 4 && road.getRoadId() == 2) {//Car moves while on road 2 without traffic light on road 2
+                    car.carMove();
+                    System.out.println("Car " + car.getId() + " is on road : " + road.getRoadId() + " on segment " + car.getPositionOnRoad());
+
+                }
 
             }
-
         }
 
     }
-
-
 }
+
+
 
 
 
