b0e4da163c3a08a5734b149c57b5b724fa138e70
Aidan <aidan.megee@my.jcu.edu.au>
2019-10-11 13:51:17 +1000

    Fix VehicleTest class to handle values outside of the vehicle class constructor. Test for getter/setter methods. Add commend idea for future to check that vehicle moves using a for loop.

--- src/Simulation/VehicleTest.java
+++ src/Simulation/VehicleTest.java
@@ -7,23 +7,28 @@ import static org.junit.jupiter.api.Assertions.*;
 class VehicleTest {
 
 
-
     @Test
     void carTest() { //Car class test for road objects and getters
         Vehicle vehicle = new Vehicle(3, 2, 3, 4, 2);
-
-        assertEquals(0, car.getPositionOnRoad());
-        assertEquals(1, car.getId());
+        vehicle.setSpeed(3);
+        vehicle.setPositionOnRoad(2);
+        vehicle.setId(3);
+        vehicle.setVehicleLength(4);
+        vehicle.setVehicleBreadth(2); //car breadth is half of its length
+        assertEquals(3, vehicle.getSpeed());
+        assertEquals(2, vehicle.getPositionOnRoad());
+        assertEquals(3, vehicle.getId());
+        assertEquals(4, vehicle.getVehicleLength());
+        assertEquals(2, vehicle.getVehicleBreadth());
 
     }
 
     @Test
-    void carMove() { //tests method to change car position from 0 to 1
-        Car car = new Car(1);
-        car.carMove();
-        assertEquals(1, car.getPositionOnRoad());
-
-
+    void vehicleMove() { //tests method to change car position from 0 to 1
+        Vehicle vehicle = new Vehicle(3, 2, 3, 4, 2);
+        vehicle.vehicleMove();
+        //for loop to call vehicleMove() method each time vehicle is on a road and traffic lights are not red/orange
+        assertEquals(1, vehicle.getPositionOnRoad());
 
 
     }
